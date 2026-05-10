0cd45f822cf2223fb89ce995d303aaf7af4853a5
hugo0515 <167876219+hugo0515@users.noreply.github.com>
2026-05-10 22:37:03 +0900

    Update preprocessing and validation split

--- proposed_AutoEncoder/config.py
+++ proposed_AutoEncoder/config.py
@@ -1,10 +1,22 @@
+import torch
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Dataset paths
-TRAIN_PATH = "dataset/KDDTrain+.txt"
-TEST_PATH = "dataset/KDDTest+.txt"
+# NSL-KDD
+
+# TRAIN_PATH = BASE_DIR / "dataset" / "NSL-KDD" / "KDDTrain+.txt"
+# TEST_PATH = BASE_DIR / "dataset" / "NSL-KDD" / "KDDTest+.txt"
+
+# #UNSW_NB15
+TRAIN_PATH = BASE_DIR / "dataset" / "UNSW_NB15" / "UNSW_NB15_training-set.csv"
+TEST_PATH = BASE_DIR / "dataset" / "UNSW_NB15" / "UNSW_NB15_testing-set.csv"
+
 
 # Model parameters
 INPUT_DIM = None  # will be set automatically after preprocessing
-LATENT_DIM = 32   # bottleneck dimension
+LATENT_DIM = 32  # bottleneck dimension
 HIDDEN_DIM1 = 128
 HIDDEN_DIM2 = 64
 DROPOUT_RATE = 0.3
@@ -15,11 +27,10 @@ NUM_EPOCHS = 20
 LEARNING_RATE = 0.001
 
 # Device configuration
-import torch
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Threshold path (to store reconstruction error threshold)
 THRESHOLD_PATH = "threshold_value.npy"
 
-#trained model path
+# trained model path
 AE_MODEL_PATH = "autoencoder_nslkdd.pth"
--- proposed_AutoEncoder/main.py
+++ proposed_AutoEncoder/main.py
@@ -4,21 +4,23 @@ from config import DEVICE
 from train import train_autoencoder
 from test import evaluate_autoencoder
 
+
 def main():
-    print(f"\n🚀 Device in use: {DEVICE}\n")
+    print(f"\n Device in use: {DEVICE}\n")
 
     model_path = "autoencoder_nslkdd.pth"
 
     # Check if model already trained
     if os.path.exists(model_path):
-        print("✅ Found existing trained model. Skipping training...\n")
+        print(" Found existing trained model. Skipping training...\n")
     else:
-        print("🧠 Training AutoEncoder model from scratch...\n")
+        print(" Training AutoEncoder model from scratch...\n")
         train_autoencoder()
-        print("✅ Model training completed.\n")
+        print(" Model training completed.\n")
 
     print("🔍 Starting evaluation on test dataset...\n")
     evaluate_autoencoder()
+
 
 if __name__ == "__main__":
     main()
